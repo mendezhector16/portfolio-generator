94e959a70ace2caf198cdafda8e567e82fa1e4c1
mendezhector16 <mendezhector16@gmail.com>
2022-06-23 20:15:01 -0500

    debugging

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const fs = require('fs');
+import { inquirer } from 'inquirer';
 
-const generatePage = require('./src/page-template.js')
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js')
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
