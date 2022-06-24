d2dbae244c5c47cef6ba51df4552eee01bad713f
mendezhector16 <mendezhector16@gmail.com>
2022-06-23 20:29:56 -0500

    downgraded the inquirer to best fit my needs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { prompt } from 'inquirer';
+const inquirer = require('inquirer');
 
 // const fs = require('fs');
 
@@ -12,11 +12,12 @@ import { prompt } from 'inquirer';
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        }
-    ])
-    .then(answers => console.log(answers));
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
